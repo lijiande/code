e532a4e3de7e7a613669ef2c6486ee1d2e750438
lijiande <lijiande@users.noreply.github.com>
2017-05-10 11:34:42 +0800

    增加邀请码实体，暂停该实体开发，留作后用

---
+++ entity/src/main/java/cn/finte/code/entity/data/Invite.java
@@ -0,0 +1,22 @@
+package cn.finte.code.entity.data;
+
+import java.util.Date;
+
+/**
+ * Created by lijian on 2017/5/10.
+ */
+public class Invite {
+
+    private String id;
+
+    private String user;
+
+    private Date createTime;
+
+    private Integer loseMinute;
+
+    private Integer times;
+
+    private String code;
+
+}
