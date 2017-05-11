7a9b46efeb494fb369affa90e8a230783714916b
lijiande <lijiande@users.noreply.github.com>
2017-05-11 15:03:08 +0800

    update method from data-con

--- service/src/main/java/cn/finte/code/service/web/DataController.java
+++ service/src/main/java/cn/finte/code/service/web/DataController.java
@@ -5,6 +5,8 @@ import cn.finte.code.core.model.Result;
 import cn.finte.code.entity.data.Code;
 import cn.finte.code.service.model.condition.CodeCondition;
 import cn.finte.code.service.service.CodeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -21,6 +23,7 @@ import javax.inject.Inject;
  */
 @RestController
 @RequestMapping("/api/dataController")
+@Api(value = "数据操作",description = "数据操作")
 public class DataController {
 
     private final Logger log = LoggerFactory.getLogger(DataController.class);
@@ -30,6 +33,7 @@ public class DataController {
     private CodeService codeService;
 
     @RequestMapping(value = "/saveData",method = RequestMethod.POST)
+    @ApiOperation(value = "保存数据",notes = "保存数据")
     public Result saveCode(@Validated @RequestBody CodeCondition condition){
         try {
             Code code = new Code();
@@ -45,5 +49,27 @@ public class DataController {
             log.error(e.getMessage(),e);
             return new Result(Result.ReturnValue.FAILURE, Constants.ERROR);
         }
+    }
+
+    @RequestMapping(value = "/findByPage",method = RequestMethod.GET)
+    @ApiOperation(value = "分页查询",notes = "分页查询")
+    public Result findByPage() {
+        return null;
+    }
+
+    @RequestMapping(value = "/delete",method = RequestMethod.DELETE)
+    @ApiOperation(value = "删除操作",notes = "删除操作")
+    public Result delete(String id) {
+        try {
+            Boolean result = codeService.deleteById(id);
+            if(result){
+                return new Result(Result.ReturnValue.SUCCESS,"");
+            }
+            return new Result(Result.ReturnValue.FAILURE, Constants.ERROR,"保存失败");
+        }catch (Exception e){
+            log.error(e.getMessage(),e);
+            return new Result(Result.ReturnValue.FAILURE, Constants.ERROR);
+        }
+
     }
 }
